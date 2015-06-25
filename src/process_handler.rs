61f783d42763eeaab66dd33766e5b1a5a4d2edef
Daniel Gondos <dan.gondos@gmail.com>
2015-06-25 17:54:47 +1000

    spawning processes from process_handler

--- src/process_handler.rs
+++ src/process_handler.rs
@@ -1,6 +1,7 @@
 use std::thread;
 use std::sync::Arc;
 use std::thread::JoinHandle;
+use std::process::Command;
 
 use config::Config;
 
@@ -16,7 +17,15 @@ impl ProcessHandler {
     pub fn execute(self) -> JoinHandle<()> {
         let config = Arc::new(self.config).clone();
         thread::spawn(move|| {
-            println!("Launching {:?} using command: {:?}, with args: {:?}, from directory: {:?}", config.name, config.cmd, config.args, config.cwd);
+            loop {
+                println!("Launching {:?} using command: {:?}, with args: {:?}, from directory: {:?}", config.name, config.cmd, config.args, config.cwd);
+                let mut child = Command::new(&config.cmd)
+                            .current_dir(&config.cwd)
+                            .spawn()
+                            .unwrap_or_else(|e| { panic!("failed to execute child: {}", e) });
+
+                child.wait().unwrap_or_else(|e| { panic!("failed to wait on child: {}", e) });
+            }
         })
     }
 }
